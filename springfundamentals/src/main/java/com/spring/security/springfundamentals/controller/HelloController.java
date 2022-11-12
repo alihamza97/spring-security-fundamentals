e461f23200634ff1641929fede192d0de3243a1e
Ali <ali.hamza@cognizant.com>
2022-11-12 17:36:57 +0100

    default authenticaion with built-in spring security dependency

---
+++ springfundamentals/src/main/java/com/spring/security/springfundamentals/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.spring.security.springfundamentals.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Spring security fundamentals";
+	}
+}
