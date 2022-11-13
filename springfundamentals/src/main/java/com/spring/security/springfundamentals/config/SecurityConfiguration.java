a567b1ad4807ad1938cbc1e5fa4f9d0ee77b6ec6
Ali <ali.hamza@cognizant.com>
2022-11-13 19:38:30 +0100

    cutomized authenticationProvider

--- springfundamentals/src/main/java/com/spring/security/springfundamentals/config/SecurityConfiguration.java
+++ springfundamentals/src/main/java/com/spring/security/springfundamentals/config/SecurityConfiguration.java
@@ -1,30 +1,42 @@
 package com.spring.security.springfundamentals.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@SuppressWarnings("deprecation")
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private MyAuthenticationProvider authenticationProvider;
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+//		// create username and password
+//		UserDetails user = User.withUsername("ali").password(passwordEncoder.encode("ali123")).authorities("read")
+//				.build();
+//		userDetailsService.createUser(user);
+//		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+//
+//	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		// create username and password
-		UserDetails user = User.withUsername("ali").password(passwordEncoder.encode("ali123")).authorities("read")
-				.build();
-		userDetailsService.createUser(user);
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-
+		auth.authenticationProvider(authenticationProvider);
 	}
 
 	@Override
@@ -34,5 +46,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().anyRequest().authenticated();
 
 	}
+
+//	@Bean
+//	public AuthenticationManager authManager(HttpSecurity http, PasswordEncoder passwordEncoder,
+//			UserDetailsService userDetailService) throws Exception {
+//		return http.getSharedObject(AuthenticationManagerBuilder.class).authenticationProvider(authenticationProvider)
+//				.build();
+//	}
+//
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http.httpBasic();
+//		http.authorizeRequests().anyRequest().authenticated();
+//		return http.build();
+//
+//	}
+
+	
 
 }
